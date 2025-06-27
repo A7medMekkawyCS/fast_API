4d05ce557aa7d5c576cd857f4c507855aa70a968
ahmed mekawy <ahmedmekawyxa@exampel.com>
2025-06-27 04:56:17 +0300

    update

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import tensorflow as tf
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 import numpy as np
@@ -6,15 +7,15 @@ import requests
 from PIL import Image
 import io
 import os
-
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.preprocessing.image import ImageDataGenerator, img_to_array, load_img
 app = Flask(__name__)
 
 # تحميل الموديل
-model = load_model('Skin Cancer8.keras')
+model = load_model('Skin cancer11.keras')
 
 # التصنيفات المتوقعة
-labels = ['Actinic keratoses', 'Basal cell carcinoma', 'Benign keratosis-like lesions',
-          'Dermatofibroma', 'Melanocytic nevi', 'Melanoma', 'Vascular lesions']
+labels = ['Benign','Malignant']
 
 # معالجة الصورة
 def prepare_image(img_url, target_size=(224, 224)): 
@@ -22,9 +23,10 @@ def prepare_image(img_url, target_size=(224, 224)):
     img = Image.open(io.BytesIO(response.content)).convert('RGB')
     img = img.resize(target_size)
     img_array = image.img_to_array(img)
-    img_array = np.expand_dims(img_array, axis=0)
-    img_array = img_array / 255.0  
+    img_array = preprocess_input(img_array)  # Normalize input for MobileNet
+    img_array = tf.expand_dims(img_array, axis=0)  # Add batch dimension
     return img_array
+
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -37,15 +39,11 @@ def predict():
     try:
         img = prepare_image(img_url)
         predictions = model.predict(img)
-        
-        predicted_index = np.argmax(predictions[0])
-        predicted_label = labels[predicted_index]
-        confidence = float(predictions[0][predicted_index])
-        
+        threshold = 0.965  # Increase the threshold to be more confident
+        predicted_label = 'Malignant' if predictions[0] >= threshold else 'Benign'
         return jsonify({
-            'predicted_label': predicted_label,
-            'confidence': confidence
-        })
+            'predicted_label': predicted_label
+            })
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
